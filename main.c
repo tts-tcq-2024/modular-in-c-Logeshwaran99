838f93512338fc2eb73828c94a2091c614fd0a81
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-08-20 06:24:25 +0530

    Reference Manual

--- main.c
+++ main.c
@@ -7,5 +7,9 @@ int main() {
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
 
+    // Set the flag to 1 when you want to print the manual for testing
+    printColorReferenceManual = 1;
+    PrintColorReferenceManual();
+
     return 0;
 }
