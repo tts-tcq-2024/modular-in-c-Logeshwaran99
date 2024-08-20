135e45d633d407e766ec442afc9d4966f90ebd0f
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-08-20 06:12:03 +0530

    Update4

---
+++ GetColorFromPairNumber.h
@@ -0,0 +1,8 @@
+#ifndef GETCOLORFROMPAIRNUMBER_H
+#define GETCOLORFROMPAIRNUMBER_H
+
+#include "ColorCode.h"
+
+ColorPair GetColorFromPairNumber(int pairNumber);
+
+#endif // GETCOLORFROMPAIRNUMBER_H
