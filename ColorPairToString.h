509ef76cb83aa996310e1e2a9e62da461484ec35
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-08-20 06:10:00 +0530

    Update2

---
+++ ColorPairToString.h
@@ -0,0 +1,8 @@
+#ifndef COLORPAIRTOSTRING_H
+#define COLORPAIRTOSTRING_H
+
+#include "ColorCode.h"
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+
+#endif // COLORPAIRTOSTRING_H
