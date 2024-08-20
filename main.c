84e44331c2720b2fb4c65f8308b5e9ced63e8b0e
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-08-20 06:26:05 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,5 @@
 #include "testColorCode.h"
 
-// Define a flag to control printing the color reference manual
-int printColorReferenceManual = 0;
-
-void PrintColorReferenceManual(void) {
-    if (!printColorReferenceManual) {
-        return;  // Do not print if the flag is not set
-    }
-
-    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
-    for (int i = 1; i <= numberOfMajorColors * numberOfMinorColors; i++) {
-        ColorPair colorPair = GetColorFromPairNumber(i);
-        ColorPairToString(&colorPair, colorPairNames);
-        printf("%d: %s\n", i, colorPairNames);
-    }
-}
-
 int main() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
