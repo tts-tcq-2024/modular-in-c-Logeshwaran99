ef49b667eedc7bebe8be704fa390105b0c99b274
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-08-20 06:18:08 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "testColorCode.h"
+
+int main() {
+    testNumberToPair(4, WHITE, BROWN);
+    testNumberToPair(5, WHITE, SLATE);
+
+    testPairToNumber(BLACK, ORANGE, 12);
+    testPairToNumber(VIOLET, SLATE, 25);
+
+    return 0;
+}
